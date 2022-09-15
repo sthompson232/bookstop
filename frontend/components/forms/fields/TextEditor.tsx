22f4b692b6106ebe1a30d89c0c77040e0d33bfa8
Sam Thompson <samthompson292@gmail.com>
2022-09-15 10:07:33 +0100

    updated tinymce toolbar and plugins

--- frontend/components/forms/fields/TextEditor.tsx
+++ frontend/components/forms/fields/TextEditor.tsx
@@ -60,8 +60,22 @@ const TextEditor = (props: PropTypes) => {
 					disabled={fetchError}
 					ref={ref}
 					id="tinymce-editor"
-					// plugins=""
-					// toolbar=""
+					init={{
+						menubar: 'edit insert format tools table',
+						menu: {
+							file: { title: 'File', items: 'newdocument restoredraft | preview | print ' },
+							edit: { title: 'Edit', items: 'undo redo | cut copy paste | selectall | searchreplace' },
+							view: { title: 'View', items: 'code | visualaid visualchars visualblocks | spellchecker | preview fullscreen' },
+							insert: { title: 'Insert', items: 'image link media template codesample inserttable | charmap emoticons hr | pagebreak nonbreaking anchor toc | insertdatetime' },
+							format: { title: 'Format', items: 'bold italic underline strikethrough superscript subscript codeformat | formats blockformats fontformats fontsizes align lineheight | forecolor backcolor | removeformat' },
+							tools: { title: 'Tools', items: 'spellchecker spellcheckerlanguage | code wordcount | preview | fullscreen' },
+							table: { title: 'Table', items: 'inserttable | cell row column | tableprops deletetable' },
+							help: { title: 'Help', items: 'help' }
+						},
+						toolbar_mode: 'wrap',
+					}}
+					plugins="preview wordcount autolink emoticons fullscreen image link lists media table"
+					toolbar="undo redo | styles | bold italic | alignleft aligncenter alignright alignjustify | outdent indent | numlist bullist | media image link | fullscreen"
 					tagName={name}
 					onEditorChange={onChange}
 					onBlur={onBlur}
